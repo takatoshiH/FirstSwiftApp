22be2ebb867992a7da79643ff1e1b46cf294fb0f
引尻貴俊 <hikijiri@hakobune.co.jp>
2020-11-06 18:50:12 +0900

    FIrstApp

---
+++ myfirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myfirstApp
+//
+//  Created by 引尻貴俊 on 2020/11/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myfirstApp/myfirstAppApp.swift
@@ -0,0 +1,23 @@
+//
+//  myfirstAppApp.swift
+//  myfirstApp
+//
+//  Created by 引尻貴俊 on 2020/11/05.
+//
+
+import SwiftUI
+
+@main
+struct myfirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct myfirstAppApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
